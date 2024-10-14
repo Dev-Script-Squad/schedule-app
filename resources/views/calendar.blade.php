f891d2def876db5bef4e27052d236573c6cdf6d5
thiagosir <thiagobento9@hotmail.com>
2024-10-14 17:01:10 -0300

    Converting calendar to TailWind CSS

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,155 +1,282 @@
-<!DOCTYPE html>
-<html>
+{{-- @extends('layouts.default')
 
-<head>
-    <meta charset='utf-8' />
-    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
-    {{-- <script src='../dist/index.global.js'></script> --}}
-    <script src='{{ asset('assets/fullcalendar/packages/core/locales-all.global.js') }}'></script>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var containerEl = document.getElementById('external-events-list');
-            new FullCalendar.Draggable(containerEl, {
-                itemSelector: '.fc-event',
-                eventData: function(eventEl) {
-                    return {
-                        title: eventEl.innerText.trim()
-                    }
+@section('content')
+    <html>
+
+    <head>
+        <meta charset='utf-8' />
+
+
+    </head>
+
+    <body>
+        <div id='wrap'>
+            <div id='external-events'>
+                <h4>Draggable Events</h4>
+                <div id='external-events-list'>
+                    <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+                        <div class='fc-event-main'>My Event 1</div>
+                    </div>
+                    <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+                        <div class='fc-event-main'>My Event 2</div>
+                    </div>
+                    <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+                        <div class='fc-event-main'>My Event 3</div>
+                    </div>
+                    <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+                        <div class='fc-event-main'>My Event 4</div>
+                    </div>
+                    <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+                        <div class='fc-event-main'>My Event 5</div>
+                    </div>
+                </div>
+
+                <p>
+                    <input type='checkbox' id='drop-remove' />
+                    <label for='drop-remove'>remove after drop</label>
+                </p>
+                <x-add-register-modal />
+            </div>
+
+            <div id='calendar-wrap'>
+                <div id='calendar' data-route-load-events="{{ route('calendar.loadEvents') }}">
+                </div>
+            </div>
+
+        </div>
+    </body>
+
+    </html>
+@endsection
+
+<script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
+<script src='{{ asset('assets/fullcalendar/packages/core/locales-all.global.js') }}'></script>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        var containerEl = document.getElementById('external-events-list');
+        new FullCalendar.Draggable(containerEl, {
+            itemSelector: '.fc-event',
+            eventData: function(eventEl) {
+                return {
+                    title: eventEl.innerText.trim()
                 }
-            });
-
-            var calendarEl = document.getElementById('calendar');
-            var loadEventsRoute = calendarEl.dataset.routeLoadEvents;
-
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                headerToolbar: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-                },
-                locale: 'pt-br',
-                navLinks: true,
-                eventLimit: true,
-                selectable: true,
-                editable: true,
-                droppable: true,
-                events: loadEventsRoute, // Usando a rota diretamente
-                drop: function(arg) {
-                    if (document.getElementById('drop-remove').checked) {
-                        arg.draggedEl.parentNode.removeChild(arg.draggedEl);
-                    }
-                },
-                eventDrop: function(event) {
-                    console.log('Evento movido:', event);
-                },
-                eventClick: function(event) {
-                    console.log('Evento clicado:', event);
-                },
-                eventResize: function(event) {
-                    console.log('Evento redimensionado:', event);
-                },
-                select: function(info) {
-                    console.log('Selecionado:', info);
-                }
-            });
-
-            calendar.render();
-
+            }
         });
 
-        // function routeEvents(route) {
-        //     document.getElementById('calendar').dataset.routeLoadEvents;
-        // }
+        var calendarEl = document.getElementById('calendar');
+        var loadEventsRoute = calendarEl.dataset.routeLoadEvents;
 
-        // console.log(routeEvents(routeLoadEvents));
-    </script>
-    <style>
-        body {
-            margin-top: 40px;
-            font-size: 14px;
-            font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-        }
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            headerToolbar: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+            },
+            locale: 'pt-br',
+            navLinks: true,
+            eventLimit: true,
+            selectable: true,
+            editable: true,
+            droppable: true,
+            events: loadEventsRoute,
+            drop: function(arg) {
+                if (document.getElementById('drop-remove').checked) {
+                    arg.draggedEl.parentNode.removeChild(arg.draggedEl);
+                }
+            },
+            eventDrop: function(event) {
+                console.log('Evento movido:', event);
+            },
+            eventClick: function(event) {
+                console.log('Evento clicado:', event);
+            },
+            eventResize: function(event) {
+                console.log('Evento redimensionado:', event);
+            },
+            select: function(info) {
+                console.log('Selecionado:', info);
+            }
+        });
 
-        #external-events {
-            position: fixed;
-            left: 20px;
-            top: 20px;
-            width: 150px;
-            padding: 0 10px;
-            border: 1px solid #ccc;
-            background: #eee;
-            text-align: left;
-        }
+        calendar.render();
 
-        #external-events h4 {
-            font-size: 16px;
-            margin-top: 0;
-            padding-top: 1em;
-        }
+    });
 
-        #external-events .fc-event {
-            margin: 3px 0;
-            cursor: move;
-        }
+</script>
 
-        #external-events p {
-            margin: 1.5em 0;
-            font-size: 11px;
-            color: #666;
-        }
+<style>
+    body {
+        margin-top: 40px;
+        font-size: 14px;
+        font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+        background-color: #666;
+    }
 
-        #external-events p input {
-            margin: 0;
-            vertical-align: middle;
-        }
+    #external-events {
+        position: fixed;
+        left: 20px;
+        top: 20px;
+        width: 150px;
+        padding: 0 10px;
+        border: 1px solid #ccc;
+        background: #eee;
+        text-align: left;
+    }
 
-        #calendar-wrap {
-            margin-left: 200px;
-        }
+    #external-events h4 {
+        font-size: 16px;
+        margin-top: 0;
+        padding-top: 1em;
+    }
 
-        #calendar {
-            max-width: 1100px;
-            margin: 0 auto;
-        }
-    </style>
-</head>
+    #external-events .fc-event {
+        margin: 3px 0;
+        cursor: move;
+    }
 
-<body>
-    <div id='wrap'>
+    #external-events p {
+        margin: 1.5em 0;
+        font-size: 11px;
+        color: #666;
+    }
 
-        <div id='external-events'>
-            <h4>Draggable Events</h4>
+    #external-events p input {
+        margin: 0;
+        vertical-align: middle;
+    }
 
-            <div id='external-events-list'>
-                <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
-                    <div class='fc-event-main'>My Event 1</div>
+    #calendar-wrap {
+        margin-left: 200px;
+    }
+
+    #calendar {
+        max-width: 1100px;
+        margin: 0 auto;
+    }
+</style> --}}
+@extends('layouts.default')
+
+@section('content')
+    <html lang="pt-BR">
+
+    <head>
+        <meta charset='utf-8' />
+    </head>
+
+    <body class="bg-gray-200">
+
+        <div id='wrap' class="">
+            <div id='external-events' class="bg-gray-300 p-4 rounded-lg shadow-lg ml-[100px] mr-[75px] mb-5">
+                <h4 class="text-lg font-semibold mb-2">Eventos</h4>
+                <div id='external-events-list'>
+                    <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+                        <div class='fc-event-main'>voslei</div>
+                    </div>
+                    <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+                        <div class='fc-event-main'>bolinha</div>
+                    </div>
+                    <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+                        <div class='fc-event-main'>natação</div>
+                    </div>
+                    <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+                        <div class='fc-event-main'>oração coletiva</div>
+                    </div>
+                    <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+                        <div class='fc-event-main'>estudar</div>
+                    </div>
                 </div>
-                <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
-                    <div class='fc-event-main'>My Event 2</div>
-                </div>
-                <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
-                    <div class='fc-event-main'>My Event 3</div>
-                </div>
-                <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
-                    <div class='fc-event-main'>My Event 4</div>
-                </div>
-                <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
-                    <div class='fc-event-main'>My Event 5</div>
+
+                <p class="mt-4">
+                    <input type='checkbox' id='drop-remove' class="mr-2" />
+                    <label for='drop-remove'>remove after drop</label>
+                </p>
+
+                <x-add-register-modal class="" />
+            </div>
+
+            <div id='calendar-wrap' class="ml-8">
+                <div id='calendar' data-route-load-events="{{ route('calendar.loadEvents') }}"
+                    class="w-full max-w-3xl mx-auto">
                 </div>
             </div>
 
-            <p>
-                <input type='checkbox' id='drop-remove' />
-                <label for='drop-remove'>remove after drop</label>
-            </p>
         </div>
+    </body>
 
-        <div id='calendar-wrap'>
-            <div id='calendar' data-route-load-events="{{ route('calendar.loadEvents') }}">
-            </div>
-        </div>
+    </html>
+@endsection
 
-    </div>
-</body>
+<script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
+<script src='{{ asset('assets/fullcalendar/packages/core/locales-all.global.js') }}'></script>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        var containerEl = document.getElementById('external-events-list');
+        new FullCalendar.Draggable(containerEl, {
+            itemSelector: '.fc-event',
+            eventData: function(eventEl) {
+                return {
+                    title: eventEl.innerText.trim()
+                }
+            }
+        });
 
-</html>
+        var calendarEl = document.getElementById('calendar');
+        var loadEventsRoute = calendarEl.dataset.routeLoadEvents;
+
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            headerToolbar: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+            },
+            locale: 'pt-br',
+            navLinks: true,
+            eventLimit: true,
+            selectable: true,
+            editable: true,
+            droppable: true,
+            events: loadEventsRoute,
+            drop: function(arg) {
+                if (document.getElementById('drop-remove').checked) {
+                    arg.draggedEl.parentNode.removeChild(arg.draggedEl);
+                }
+            },
+            eventDrop: function(event) {
+                console.log('Evento movido:', event);
+            },
+            eventClick: function(event) {
+                console.log('Evento clicado:', event);
+            },
+            eventResize: function(event) {
+                console.log('Evento redimensionado:', event);
+            },
+            select: function(info) {
+                console.log('Selecionado:', info);
+            }
+        });
+
+        calendar.render();
+    });
+</script>
+
+<style>
+    body {
+        margin-top: 40px;
+        font-size: 14px;
+        font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+    }
+
+    #external-events .fc-event {
+        margin: 3px 0;
+        cursor: move;
+    }
+
+    #calendar-wrap {
+        /* margin-left: 20px; */
+    }
+
+    #calendar {
+        max-width: 1100px;
+        margin: 0 auto;
+    }
+</style>
